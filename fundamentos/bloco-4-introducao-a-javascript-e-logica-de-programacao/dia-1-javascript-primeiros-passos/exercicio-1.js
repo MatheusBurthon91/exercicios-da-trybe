7bdc73a51e545b675f85d684340cd34691e35d10
MatheusBurthon91 <matatiaspatrizio@hotmail.com>
2021-07-28 18:49:18 -0300

    adicionado os exercicios 1 e o resumo do bloco.

---
+++ fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-1-javascript-primeiros-passos/exercicio-1.js
@@ -0,0 +1,14 @@
+// fazer um programa que vai conter as operações matematicas basicas com as variaveis a e b, as operações serão + - * / %
+let a = 10;
+let b = 5;
+
+let soma = a + b;
+console.log(soma);
+let subtracao = a - b;
+console.log(subtracao);
+let multiplicacao = a * b;
+console.log(multiplicacao);
+let divisao = a / b;
+console.log(divisao);
+let modulo = a % b;
+console.log(modulo);
