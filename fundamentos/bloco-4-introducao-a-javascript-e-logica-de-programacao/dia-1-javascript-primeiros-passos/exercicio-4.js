72e0886771f6952e8aae720c3e15a8f2614b5caa
MatheusBurthon91 <matatiaspatrizio@hotmail.com>
2021-07-28 19:04:16 -0300

    feito o exercicio 4.

---
+++ fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-1-javascript-primeiros-passos/exercicio-4.js
@@ -0,0 +1,11 @@
+// fazer um programa que ao declarar uma variavel, imprima positive, se o valor for positivo, negative se for negativo
+// e imprima zero se for zero.
+let numero = 0;
+
+if (numero > 0) {
+    console.log('positive');
+} else if (numero < 0) {
+    console.log('negative');
+} else if ( numero === 0) {
+    console.log('zero');
+}
