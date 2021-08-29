417ef0e9bddd04859c73c1b2dcc3b827a24c54b3
MatheusBurthon91 <matatiaspatrizio@hotmail.com>
2021-08-28 21:49:10 -0300

    adicionando um comentario.

--- funcoes-for-in-objetos/parte-2/exercicio2-bonus-bloco4.js
+++ funcoes-for-in-objetos/parte-2/exercicio2-bonus-bloco4.js
@@ -1,8 +1,7 @@
 let vector = [[1, 2], [3,4,5,6], [7,8,9,10]];
-console.log(newVector);
 
 function arrayOfNumbers(vector) {
-  let newVector = vector.flat();
+  let newVector = vector.flat(); // metodo que deixa o array em uma dimensão.
   let vectorPar = [];
   for(let index = 0; index < newVector.length; index += 1) {
     if(newVector[index] % 2 === 0) {
